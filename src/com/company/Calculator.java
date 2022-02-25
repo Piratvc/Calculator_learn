33160f5b5ffb79e076fd5cbf99eb452de11e2cd7
Piratvc <piratvc@gmail.com>
2022-02-25 17:44:07 +0300

    1

--- src/com/company/Calculator.java
+++ src/com/company/Calculator.java
@@ -9,14 +9,16 @@ public class Calculator {
     BinaryOperator<Integer> multiply = (x, y) -> x * y;
     //BinaryOperator<Integer> devide = (x, y) -> x / y;
 
-    BinaryOperator<Integer> devide = (x, y) -> y == 0 ? 0 : x / y;
+
+    BinaryOperator<Integer> devide = (x, y) -> y != 0 ? x / y : y;
 
 
-    UnaryOperator<Integer> pow = x -> x * x;
-    UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
 
-    Predicate<Integer> isPositive = x -> x > 0;
-    Consumer<Integer> println = System.out::println;
+        UnaryOperator<Integer> pow = x -> x * x;
+        UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
+
+        Predicate<Integer> isPositive = x -> x > 0;
+        Consumer<Integer> println = System.out::println;
 
 
 }
